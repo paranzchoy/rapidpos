baf35cda56c11af24d7ac0a0240435d9795fa25e
frappe <joyjoyolata@gmail.com>
2021-05-27 08:38:56 +0000

    Replaced posapp.py to customapp.py for get_bank_names() method

--- posawesome/public/js/posapp/components/pos/PaymentsDebitCard.vue
+++ posawesome/public/js/posapp/components/pos/PaymentsDebitCard.vue
@@ -284,7 +284,7 @@ export default {
      get_bank_names_data() {
       const vm = this;
       frappe.call({
-        method: 'posawesome.posawesome.api.posapp.get_bank_names_data',
+        method: 'posawesome.posawesome.api.custom_posapp.get_bank_names_data',
         args: {},
         async: true,
         callback: function (r) {
