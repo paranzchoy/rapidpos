263852c76cf6bf288e8f63a83b621ce0d078aa89
elliegoldstein8 <joyjoyolata@gmail.com>
2021-08-18 08:44:16 +0000

    Changed logo and size for Closing Dialog

--- posawesome/public/js/posapp/components/pos2/ClosePosShift2.vue
+++ posawesome/public/js/posapp/components/pos2/ClosePosShift2.vue
@@ -177,7 +177,7 @@
            <template>
              <v-row>
                 <v-col cols="6">
-                  <img src="/assets/rapidposcustom/images/TacurongLogo.png" style="height:90%; width:60%;">
+                  <img src="/assets/rapidposcustom/images/MarcellinAggregateLogo.png" style="height:70%; width:80%;">
                 </v-col>
                 <v-col cols="6">
                   <v-data-table
