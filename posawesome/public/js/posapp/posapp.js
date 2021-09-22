0246ba8f0432cb74abd5d272e7a3bd6db5dbe429
frappe <frappe@ubuntu.vm>
2021-09-22 11:50:27 +0000

    Fixed POS Closing and Opening Shift duplicated amend_from (again)

--- posawesome/public/js/posapp/posapp.js
+++ posawesome/public/js/posapp/posapp.js
@@ -5,26 +5,6 @@ frappe.provide('frappe.PosApp');
 
 
 frappe.PosApp.posapp = class {
-<<<<<<< HEAD
-        constructor({ parent }) {
-                this.$parent = $(document);
-                this.page = parent.page;
-                this.make_body();
-        }
-        make_body() {
-                this.$el = this.$parent.find('.main-section');
-                this.vue = new Vue({
-                        vuetify: new Vuetify(),
-                        el: this.$el[0],
-                        data: {
-                        },
-                        render: h => h(Home),
-                });
-        }       
-        setup_header() {
-                
-        }
-=======
     constructor({ parent }) {
         this.$parent = $(document);
         this.page = parent.page;
@@ -45,5 +25,4 @@ frappe.PosApp.posapp = class {
 
     }
     
->>>>>>> 2343a1e27667b56ffe54b0d96bb32b10a69cf8db
 };
